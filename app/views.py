84e11186736825c9507fadecf952b82572c5a644
Tito-74 <kipkirui133@gmail.com>
2021-09-10 22:52:10 +0300

    adding article page

--- app/views.py
+++ app/views.py
@@ -9,3 +9,11 @@ def index():
     View root page function that returns the index page and its data
     '''
     return render_template('index.html')
+
+@app.route('/article/<article_id>')
+def movie(article_id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    return render_template('article.html',id = article_id)
