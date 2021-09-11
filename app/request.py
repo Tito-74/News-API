ada825a12f53d21ee971b79cf0cf0873245a72c3
Tito-74 <kipkirui133@gmail.com>
2021-09-11 14:05:26 +0300

    modified url

--- app/request.py
+++ app/request.py
@@ -52,3 +52,45 @@ def process_results(source_news_list):
         source_results.append(source_object)
 
     return source_results
+# def get_sources(category):
+#     '''
+#     Function that gets the json response to our url request
+#     '''
+#     get_source_url = base_url.format(category,api_key)
+
+#     with urllib.request.urlopen(get_source_url) as url:
+#         get_sources_data = url.read()
+#         get_sources_response = json.loads(get_sources_data)
+
+#         source_results = None
+
+#         if get_sources_response['sources']:
+#             source_results_list = get_sources_response['sources']
+#             source_results = process_results(source_results_list)
+
+
+#     return source_results
+
+# def process_results(source_news_list):
+#     '''
+#     Function  that processes the source result and transform them to a list of Objects
+
+#     Args:
+#         source_list: A list of dictionaries that contain source details
+
+#     Returns :
+#         source_results: A list of source objects
+#     '''
+#     source_results = []
+#     for source_item in source_news_list:
+#         id = source_item .get('id')
+#         name = source_item .get('name')
+#         description = source_item .get('description')
+#         url = source_item .get('url')
+#         category = source_item .get('category')
+#         country = source_item .get('country')
+
+#         source_object = Source(id,name,description,url,category,country)
+#         source_results.append(source_object)
+
+#     return source_results
--- app/views.py
+++ app/views.py
@@ -12,6 +12,15 @@ def index():
     major_source = get_sources('source')
     title = 'Home - Welcome to News Website Online'
     return render_template('index.html', title=title, major_source= major_source)
+    # major_source = get_sources('general')
+    # major_source = get_sources('entertainment')
+    # major_source = get_sources('business')
+    # major_source = get_sources('health')
+    # major_source = get_sources('sports')
+    # major_source = get_sources('technology')
+    # title = 'Home - Welcome to News Website Online'
+    # return render_template('index.html', title=title, major_source= major_source)
+
 
 @app.route('/article/<article_id>')
 def article(article_id):
